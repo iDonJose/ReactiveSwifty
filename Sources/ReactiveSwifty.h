b345293af8b6ec7e9d726b57b4055098daa9dbc5
iDonJose <donor.develop@gmail.com>
2018-11-28 14:16:12 +0100

    Removed unused UIKit import

--- Sources/ReactiveSwifty.h
+++ Sources/ReactiveSwifty.h
@@ -6,7 +6,6 @@
 //  Copyright © 2018 iDonJose. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 
 //! Project version number for ReactiveSwifty.
 FOUNDATION_EXPORT double ReactiveSwiftyVersionNumber;
